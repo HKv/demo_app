428f37f866498c4ddf7cf555bf41ac64a794a6e7
HKv <kavayakiintheskywithdiamond@gmail.com>
2014-01-28 01:17:58 +0900

    Initial commit

---
+++ vendor/bundle/ruby/2.1.0/gems/pg-0.12.2/ext/siteconf20140128-71871-5fzmnr.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140128-71871-2navkg"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
